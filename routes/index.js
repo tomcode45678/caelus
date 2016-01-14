89876d41b6563fa15eb1cd196d7a163a12c2ab8e
thomasjohnstapleton <thomasstapleton0@gmail.com>
2016-01-14 23:27:37 +0000

    Updated project to node express app

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Build your subscription package' });
+});
+
+module.exports = router;
