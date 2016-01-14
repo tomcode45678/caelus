81fc9b1739ad1160c788d5a8bc72f9b2acdab44d
thomasjohnstapleton <thomasstapleton0@gmail.com>
2016-01-14 23:31:16 +0000

    Added route middleware to set customerId cookie

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/', function setCustomerId(req, res, next) {
+  if (req.cookies.customerId) {
+    req.customerId = req.cookies.customerId;
+  }
+  res.cookie('customerId', 1231, { maxAge: 900000, httpOnly: true });
+  req.customerId = req.cookies.customerId;
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var customerId = req.customerId;
+  console.log(customerId);
   res.render('index', { title: 'Build your subscription package' });
 });
 
