c3927c628e7b39db5327823eb10c28fe00aa1bea
thomasjohnstapleton <thomasstapleton0@gmail.com>
2016-01-20 21:37:42 +0000

    Updated catalogue service tests

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,3 +1,9 @@
 exports.getProducts = function (locationId, callback) {
-  
+  var err = false;
+  if (err) {
+    callback(err);
+  }
+  else {
+    callback(null, locationId);
+  }
 }
--- tests/services/catalogueService.spec.js
+++ tests/services/catalogueService.spec.js
@@ -7,6 +7,12 @@ describe('catalogueService', function() {
   });
 
   describe('getProducts', function () {
+    var mockLocationId = 'GB_MERSY_LIVERPOOL';
+
+    beforeEach(function () {
+      this.mockCallback = function () {};
+    });
+
     it('should equal a function', function () {
       expect(catalogueService.getProducts).toEqual(jasmine.any(Function));
     });
@@ -14,11 +20,17 @@ describe('catalogueService', function() {
     it('should call the products model to get the available products', function () {
       spyOn(productsModel, 'getProducts');
 
-      var mockLocationId = 'GB_MERSY_LIVERPOOL';
-
       catalogueService.getProducts(mockLocationId);
 
       expect(productsModel.getProducts).toHaveBeenCalledWith(mockLocationId, jasmine.any(Function));
+    });
+
+    it('should call the callback with an error if one is passed', function () {
+      spyOn(this, 'mockCallback');
+
+      catalogueService.getProducts(mockLocationId, this.mockCallback);
+
+      expect(this.mockCallback).toHaveBeenCalled();
     });
   });
 });
