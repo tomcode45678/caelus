853bfebfb8f74f74d1a118782f1102b6a23c9793
thomasjohnstapleton <thomasstapleton0@gmail.com>
2016-01-21 23:52:23 +0000

    Updated user data to include location id's

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,12 @@
 var fs = require('fs');
-var USERS_PATH = 'data/users.json';
+var USERS_DATA_PATH = 'data/users.json';
 
 exports.getUser = function (customerId, callback) {
   this.getUsers(this.findUser.bind(null, customerId, callback));
 }
 
 exports.getUsers = function (callback) {
-  fs.readFile(USERS_PATH, 'utf8', function (err, data) {
+  fs.readFile(USERS_DATA_PATH, 'utf8', function (err, data) {
     if (err) return callback(err);
     callback(null, JSON.parse(data).users);
   });
