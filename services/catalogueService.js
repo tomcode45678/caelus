f36b293909d259383227b9b165fb92dde4ec12eb
thomasjohnstapleton <thomasstapleton0@gmail.com>
2016-01-23 15:44:11 +0000

    Updated test coverage for catalogue service

--- services/catalogueService.js
+++ services/catalogueService.js
@@ -1,8 +1,46 @@
 var productsModel = require('../models/productsModel');
 
-exports.getProducts = function(locationId, callback) {
-  productsModel.getProducts(locationId, function (err, products) {
+/**
+ * Class which builds a custom catalogue, using products and category data
+ */
+exports.cachedProducts = [];
+
+exports.getLocationBasedProducts = function(locationId, callback) {
+  this.filterProducts(locationId, function (err, products) {
     if (err) return callback(err);
-    callback(products);
+    callback(null, products);
   });
+};
+
+exports.getAvailableProducts = function (callback) {
+  var filter = {key: 'available', value: true};
+  this.filterProducts(filter, callback);
+}
+
+exports.filterProducts = function (filter, callback) {
+  if (!filter || !filter.key || !filter.value) return callback('filter: ' + filter);
+
+  if (this.cachedProducts.length) {
+    this.filter(filter, callback);
+  } else {
+    productsModel.getProducts(function (products) {
+      this.cachedProducts = products;
+      this.filter(filter, callback);
+    });
+  }
+}
+
+exports.filter = function (filter, callback) {
+  for (var i = 0, productsLength = this.cachedProducts.length; i < productsLength; i++) {
+    var product = this.cachedProducts[i];
+    if (product[filter.key] !== filter.value) {
+      this.cachedProducts.splice(i, 1);
+    }
+  }
+  return callback(null, this.cachedProducts);
+}
+
+// Format data for view
+exports.buildCatalogue = function () {
+
 };
--- tests/services/catalogueService.spec.js
+++ tests/services/catalogueService.spec.js
@@ -2,35 +2,131 @@ var catalogueService = require('../../services/catalogueService');
 var productsModel = require('../../models/productsModel');
 
 describe('catalogueService', function() {
+
   it('should be defined', function () {
     expect(catalogueService).toBeDefined();
   });
 
-  describe('getProducts', function () {
+  describe('getLocationBasedProducts', function () {
     var mockLocationId = 'GB_MERSY_LIVERPOOL';
 
+    it('should equal a function', function () {
+      expect(catalogueService.getLocationBasedProducts).toEqual(jasmine.any(Function));
+    });
+
+    it('should call a filter products function', function () {
+      spyOn(catalogueService, 'filterProducts');
+
+      catalogueService.getLocationBasedProducts(mockLocationId);
+
+      expect(catalogueService.filterProducts).toHaveBeenCalledWith(mockLocationId, jasmine.any(Function));
+    });
+  });
+
+  describe('getAvailableProducts', function () {
     beforeEach(function () {
       this.mockCallback = function () {};
     });
 
     it('should equal a function', function () {
-      expect(catalogueService.getProducts).toEqual(jasmine.any(Function));
+      expect(catalogueService.getAvailableProducts).toEqual(jasmine.any(Function));
     });
 
-    it('should call the products model to get the available products', function () {
-      spyOn(productsModel, 'getProducts');
+    it('should call a filter function', function () {
+      var expectedFilter = {key: 'available', value: true};
 
-      catalogueService.getProducts(mockLocationId);
+      spyOn(catalogueService, 'filterProducts');
 
-      expect(productsModel.getProducts).toHaveBeenCalledWith(mockLocationId, jasmine.any(Function));
+      catalogueService.getAvailableProducts(this.mockCallback);
+
+      expect(catalogueService.filterProducts).toHaveBeenCalledWith(expectedFilter, this.mockCallback);
+    });
+  });
+
+  describe('filterProducts', function () {
+    beforeEach(function () {
+      this.mockCallback = function () {};
     });
 
-    it('should call the callback with an error if one is passed', function () {
+    it('should equal a function', function () {
+      expect(catalogueService.filterProducts).toEqual(jasmine.any(Function));
+    });
+
+    it('should pass an error message into the callback a incorrect filter is not provided', function () {
+      var filter;
+      var expectedErrorMessage = 'filter: ' + filter;
+
       spyOn(this, 'mockCallback');
 
-      catalogueService.getProducts(mockLocationId, this.mockCallback);
+      catalogueService.filterProducts(filter, this.mockCallback);
 
-      expect(this.mockCallback).toHaveBeenCalled();
+      expect(this.mockCallback).toHaveBeenCalledWith(expectedErrorMessage);
+    });
+
+    it('should call a filter method if there are products available', function () {
+      var filter = {key: 'compare', value: 'mustBeThisValue'};
+      var mockProduct = {title: "something"};
+      catalogueService.cachedProducts = [mockProduct, mockProduct];
+
+      spyOn(catalogueService, 'filter');
+
+      catalogueService.filterProducts(filter, this.mockCallback);
+
+      expect(catalogueService.filter).toHaveBeenCalledWith(filter, this.mockCallback);
+    });
+
+    it('should call the populate the cached products if there are none', function () {
+      var expectedFilter = {key: 'compare', value: 'mustBeThisValue'};
+      catalogueService.cachedProducts = [];
+
+      spyOn(productsModel, 'getProducts');
+
+      catalogueService.filterProducts(expectedFilter, this.mockCallback);
+
+      expect(productsModel.getProducts).toHaveBeenCalledWith(jasmine.any(Function));
+    });
+  });
+
+  describe('filter', function () {
+    beforeEach(function () {
+      this.mockCallback = function () {};
+    });
+
+    it('should equal a function', function () {
+      expect(catalogueService.filter).toEqual(jasmine.any(Function));
+    });
+
+    it('should pass the filtered products into the callback', function () {
+      var productToReturn = {
+        "id": 1,
+        "category": 1,
+        "name": "Arsenal TV",
+        "available": 1,
+        "locationId": "GB_EN_LONDON"
+      };
+
+      var mockProducts = [productToReturn, {
+        "id": 2,
+        "category": 1,
+        "name": "Liverpool TV",
+        "available": 1,
+        "locationId": "GB_EN_LIVERPOOL"
+      }];
+
+      var filter = {key: 'locationId', value: 'GB_EN_LONDON'};
+      catalogueService.cachedProducts = mockProducts;
+
+      spyOn(this, 'mockCallback');
+
+      catalogueService.filter(filter, this.mockCallback);
+
+      expect(this.mockCallback).toHaveBeenCalledWith(null, [productToReturn]);
+    });
+  });
+
+  describe('buildCatalogue', function () {
+    it('should equal a function', function () {
+      expect(catalogueService.buildCatalogue).toEqual(jasmine.any(Function));
     });
   });
 });
