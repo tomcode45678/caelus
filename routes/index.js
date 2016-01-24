d71ee3cc49373967bed8ebcd54b18a4c23f63b97
thomasjohnstapleton <thomasstapleton0@gmail.com>
2016-01-24 20:13:08 +0000

    Updated routes to use Promises

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var Promise = require('promise');
 var cls = require('../services/customerLocationService');
+var catalogueService = require('../services/catalogueService');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  cls.getCustomerLocation(req.cookies.customerId, function (err, location) {
-    console.log(location);
+  var promise = new Promise(function (resolve, reject) {
+    cls.getCustomerLocation(req.cookies.customerId, function (err, location) {
+      if (location) {
+        resolve(location);
+      }
+
+      if (err) {
+        reject(err);
+      }
+    });
+  })
+  .then(function (location) {
+    catalogueService.getLocationBasedProducts(location, function (err, products) {
+      return products;
+    });
+  }, function(error) {
+    console.log("Failed!", error);
+  })
+  .then(function (products) {
+    console.log(products);
     res.render('index', { title: 'Build your subscription package' });
+  }, function(error) {
+    console.log("Failed!", error);
   });
 });
 
